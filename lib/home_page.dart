5117f26b798b91afea8394bcc9e5432e24fea88c
12kishansingh <ks888singh@gmail.com>
2025-01-09 15:35:03 +0530

    adding save and cancel button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habit_heat_map/components/my_drawer.dart';
+import 'package:habit_heat_map/database/habit_database.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,7 +19,39 @@ class _HomePageState extends State<HomePage> {
         builder: (context) => AlertDialog(
               content: TextField(
                 controller: textController,
+                decoration: const InputDecoration(
+                  hintText: "Create a new habit",
+                ),
               ),
+              actions: [
+                //save button
+                MaterialButton(
+                  onPressed: () {
+                    // get the new habit name
+                    String newHabitName = textController.text;
+
+                    // save to db
+                    context.read<HabitDatabase>().addHabit(newHabitName);
+
+                    // pop box
+                    Navigator.pop(context);
+                    // clear controller
+                    textController.clear();
+                  },
+                  child: const Text('Save'),
+                ),
+                // cancel button
+                MaterialButton(
+                  onPressed: () {
+                    //pop box
+                    Navigator.pop(context);
+                    //clear controller
+                    textController.clear();
+
+                  },
+                  child: const Text('Cancel'),
+                ),
+              ],
             ));
   }
 
