3ce605d13779074ae9d4f7791e2eea312d267a6c
12kishansingh <ks888singh@gmail.com>
2025-01-09 12:04:56 +0530

    added switch for dark mode and light mode

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:habit_heat_map/theme_provider.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatefulWidget{
-    const HomePage({super.key});
-    @override
-  State<HomePage> createState()=>_HomePageState();
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-    }
-    class _HomePageState extends State<HomePage>{
-        @override
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        child: Center(
+          child: Switch(
+            value: Provider.of<ThemeProvider>(context).isdarkmode,
+            onChanged: (value) =>
+                Provider.of<ThemeProvider>(context, listen: false)
+                    .toggleTheme(),
+          ),
+        ),
+      ),
+    );
   }
-    }
+}
